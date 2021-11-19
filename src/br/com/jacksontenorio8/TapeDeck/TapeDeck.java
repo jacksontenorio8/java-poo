ef51f250cad903845a5d6ecf76f051d5cf49e77e
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-11-19 14:06:12 -0300

    TapeDeck.java
    Programa criado para praticar orientação  a objeto.

---
+++ src/br/com/jacksontenorio8/TapeDeck/TapeDeck.java
@@ -0,0 +1,9 @@
+package br.com.jacksontenorio8.TapeDeck;
+
+public class TapeDeck {
+    boolean canRecord = false;
+    void playTape(){
+        System.out.println("tape playing");
+    }
+    void 
+}
