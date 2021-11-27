4197231cc8fb9c168be901babb6e3b50e21e1c2e
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-11-27 16:23:14 -0300

    Update

--- src/br/com/jacksontenorio8/TapeDeck/TapeDeck.java
+++ src/br/com/jacksontenorio8/TapeDeck/TapeDeck.java
@@ -1,9 +1,14 @@
 package br.com.jacksontenorio8.TapeDeck;
 
 public class TapeDeck {
+    
     boolean canRecord = false;
+    
     void playTape(){
         System.out.println("tape playing");
     }
-    void 
+    
+    void recordTape(){
+    	System.out.println("tape recording");
+    } 
 }
