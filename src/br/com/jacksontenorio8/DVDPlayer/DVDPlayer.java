b72eb2e70f30e7f883841f61bce4a724f5764ab5
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-11-30 09:33:52 -0300

    Update em DVDPlayer.java
    Criei um método..

--- src/br/com/jacksontenorio8/DVDPlayer/DVDPlayer.java
+++ src/br/com/jacksontenorio8/DVDPlayer/DVDPlayer.java
@@ -1,8 +1,13 @@
 	class DVDPlayer {
 	
 		boolean canRecord = false;
-		
+
+		void playDVD(){
+			System.out.println("DVD playning");
+		}
+
 		void recordDVD() {
+
 			System.out.println("DVD recording");
 		}
 	}
