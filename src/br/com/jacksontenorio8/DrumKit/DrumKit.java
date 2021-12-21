793ac9ee37183a36462a8513ab5a211c5d982a0e
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-12-21 15:01:11 -0300

    Update em DrunKit

--- src/br/com/jacksontenorio8/DrumKit/DrumKit.java
+++ src/br/com/jacksontenorio8/DrumKit/DrumKit.java
@@ -1,4 +1,15 @@
 package br.com.jacksontenorio8.DrumKit;
 
-public class DrumKit {
+class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
 }
--- src/br/com/jacksontenorio8/DrumKit/DrumkitTest.java
+++ src/br/com/jacksontenorio8/DrumKit/DrumkitTest.java
@@ -1,4 +1,16 @@
 package br.com.jacksontenorio8.DrumKit;
 
 public class DrumkitTest {
+    public static void main(String[] args) {
+
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+
+    }
 }
