8a8fd2d0d5f7bb211925f6927fc88ed3210497a8
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-01-27 16:57:46 -0300

    Criei a classe Cachorro.java

---
+++ src/br/com/jacksontenorio8/variaveis/Cachorro.java
@@ -0,0 +1,18 @@
+package br.com.jacksontenorio8.variaveis;
+
+public class Cachorro {
+    String nome;
+
+    public static void main(String[] args){
+        //Cria um objeto Cao e o acessa
+        Cachorro cachorro1 = new Cachorro();
+        cachorro1.late();
+        cachorro1.nome = Sheik;
+
+        //Agora uma matriz Cachorro
+        Cachorro[] meusCachorros = new Cachorro[3];
+
+        //E coloca alguns cães nela
+        
+    }
+}
